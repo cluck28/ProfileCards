9cb6dfa2eac0298b415b3cf444a41edfa9d13a85
cluck28 <csluciuk@gmail.com>
2019-10-05 13:44:50 -0400

    Basic app and unit tests

---
+++ App/project/tests/test.py
@@ -0,0 +1,35 @@
+# project/test.py
+
+import unittest
+
+from project import app
+
+class ProjectTests(unittest.TestCase):
+    '''
+    Run tests on my app
+    '''
+
+    def setUp(self):
+        '''
+        Execute before each test
+        '''
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+        self.assertEquals(app.debug, False)
+
+    def tearDown(self):
+        '''
+        Executed after each test
+        '''
+        pass
+
+    def test_index(self):
+        '''
+        Test landing page
+        '''
+        response = self.app.get('/',follow_redirects = True)
+        self.assertIn(b'About', response.data)
+
+if __name__ == "__main__":
+    unittest.main()
