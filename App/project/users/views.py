b6de0ac7a52c1d292648329e324c4fd464a6ef3d
cluck28 <csluciuk@gmail.com>
2019-10-06 14:08:15 -0400

    added password reset

--- App/project/users/views.py
+++ App/project/users/views.py
@@ -14,7 +14,7 @@ from itsdangerous import URLSafeTimedSerializer
 from datetime import datetime
 
 from project import db, mail, app
-from .forms import RegisterForm, LoginForm
+from .forms import RegisterForm, LoginForm, EmailForm, PasswordForm
 from project.models import User
 
 ######
@@ -40,6 +40,17 @@ def send_confirmation_email(user_email):
         'email_confirmation.html',
         confirm_url=confirm_url)
     send_email('Confirm Your Email Address', [user_email], html)
+
+def send_password_reset_email(user_email):
+    password_reset_serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    password_reset_url = url_for(
+        'users.reset_with_token',
+        token = password_reset_serializer.dumps(user_email, salt='password-reset-salt'),
+        _external=True)
+    html = render_template(
+        'email_password_reset.html',
+        password_reset_url=password_reset_url)
+    send_email('Password Reset Requested', [user_email], html)
 
 users_blueprint = Blueprint('users', __name__)
 
@@ -106,3 +117,49 @@ def confirm_email(token):
         db.session.commit()
 
     return redirect(url_for('evaluations.index'))
+
+@users_blueprint.route('/reset', methods=["GET", "POST"])
+def reset():
+    form = EmailForm()
+    if form.validate_on_submit():
+        try:
+            user = User.query.filter_by(email=form.email.data).first_or_404()
+        except:
+            flash('Invalid email address!', 'error')
+            return render_template('password_reset_email.html', form=form)
+
+        if user.email_confirmed:
+            send_password_reset_email(user.email)
+            flash('Please check your email for a password reset link.', 'success')
+        else:
+            flash('Your email address must be confirmed before attempting a password reset. Email resent', 'error')
+            send_confirmation_email(new_user.email) #Resend confirmation email
+        return redirect(url_for('users.login'))
+
+    return render_template('password_reset_email.html', form=form)
+
+@users_blueprint.route('/reset/<token>', methods=["GET", "POST"])
+def reset_with_token(token):
+    try:
+        password_reset_serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+        email = password_reset_serializer.loads(token, salt='password-reset-salt', max_age=600)#10 minutes
+    except:
+        flash('The password reset link is invalid or has expired.', 'error')
+        return redirect(url_for('users.login'))
+
+    form = PasswordForm()
+
+    if form.validate_on_submit():
+        try:
+            user = User.query.filter_by(email=email).first_or_404()
+        except:
+            flash('Invalid email address!', 'error')
+            return redirect(url_for('users.login'))
+
+        user.password = form.password.data
+        db.session.add(user)
+        db.session.commit()
+        flash('Your password has been updated!', 'success')
+        return redirect(url_for('users.login'))
+
+    return render_template('reset_password_with_token.html', form=form, token=token)
