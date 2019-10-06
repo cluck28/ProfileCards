7c8eb395dfe3fdd15bb866de310cd4b97d67b176
cluck28 <csluciuk@gmail.com>
2019-10-06 10:51:49 -0400

    added password hashing

--- App/project/__init__.py
+++ App/project/__init__.py
@@ -9,11 +9,13 @@ Created on Thu Jan 18 12:56:08 2018
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__, instance_relative_config = True)
 app.config.from_pyfile('flask.cfg')
 
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
